0143808dab1ca29d78688434de3bc46c350f68ee
Ramisa Chowdhury <rrchow97@gmail.com>
2021-06-18 15:23:33 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import WebGL from './WebGL'
+
+export const App = (props) => {
+  return (
+    <>
+      <h1>Reactive Programming</h1>
+      <p>We specialize in React, Typescript, React Native, Frontend Testing Strategies, React Native, Electron, and Functional+Reactive Programming Paradigms.</p>
+      <p>Contact us at contact@averylane.dev.</p>
+      <WebGL />
+    </>
+  )
+}
+
+export default App
