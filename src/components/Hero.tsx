353e42a99ff93fe11fedfbcb2948e9a161044539
Ramisa Chowdhury <rrchow97@gmail.com>
2021-06-21 18:35:10 -0500

    Added demo sections with text

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Hero = (props) => {
+  return (
+    <section className="hero">
+      <div className="content">
+        <h1>Big ideas need big mediums</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
